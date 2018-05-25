0f428b161c8119d4120338f1e9bd75c410a244fc
Alex Hinson <alex@teamairship.com>
2018-05-25 07:27:55 -0400

    Remove comments

--- android/src/main/java/com/airship/customwebview/VideoWebChromeClient.java
+++ android/src/main/java/com/airship/customwebview/VideoWebChromeClient.java
@@ -57,27 +57,16 @@ public class VideoWebChromeClient extends WebChromeClient {
     WritableMap params = Arguments.createMap();
 
     sendEvent(mReactContext, "VideoWillEnterFullScreen", params);
-    // Store the view and it's callback for later, so we can dispose of them correctly
+    // Store the view and it's callback for later, so we can dispose of them
+    // correctly
     mVideoView = view;
     mCustomViewCallback = callback;
 
     view.setBackgroundColor(Color.BLACK);
-    //mVideoView.setRotation(90);
     mActivity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-    //this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-    /*mVideoView.setKeepScreenOn(true);
-    mActivity.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-    mActivity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);*/
-    // paramsNotFullscreen= mVideoView.getLayoutParams();
-    //LayoutParams params = new LayoutParams(paramsNotFullscreen);
-    //params.setMargins(0, 0, 0, 0);
-    //params.height=ViewGroup.LayoutParams.MATCH_PARENT;
-    //params.width=ViewGroup.LayoutParams.MATCH_PARENT;
-    //params.addRule(RelativeLayout.CENTER_IN_PARENT);
-    //mVideoView.setLayoutParams(params);
     getRootView().addView(mVideoView, FULLSCREEN_LAYOUT_PARAMS);
 
-   // ((View) mWebView.getRootView()).setVisibility(View.GONE);
+    // ((View) mWebView.getRootView()).setVisibility(View.GONE);
     isVideoFullscreen = true;
   }
 
@@ -119,9 +108,12 @@ public class VideoWebChromeClient extends WebChromeClient {
   }
 
   /**
-   * Notifies the class that the back key has been pressed by the user.
-   * This must be called from the Activity's onBackPressed(), and if it returns false, the activity itself should handle it. Otherwise don't do anything.
-   * @return Returns true if the event was handled, and false if was not (video view is not visible)
+   * Notifies the class that the back key has been pressed by the user. This must
+   * be called from the Activity's onBackPressed(), and if it returns false, the
+   * activity itself should handle it. Otherwise don't do anything.
+   * 
+   * @return Returns true if the event was handled, and false if was not (video
+   *         view is not visible)
    */
   public boolean onBackPressed() {
     if (isVideoFullscreen) {
@@ -132,74 +124,3 @@ public class VideoWebChromeClient extends WebChromeClient {
     }
   }
 }
-
-// package com.ovoenergy.customwebview;
-
-// import android.app.Activity;
-// import android.graphics.Color;
-// import android.view.Gravity;
-// import android.view.View;
-// import android.view.ViewGroup;
-// import android.webkit.WebChromeClient;
-// import android.webkit.WebView;
-// import android.widget.FrameLayout;
-
-// import static android.view.ViewGroup.LayoutParams;
-
-// /**
-//  * Provides support for full-screen video on Android
-//  */
-// public class VideoWebChromeClient extends WebChromeClient {
-
-//   private final FrameLayout.LayoutParams FULLSCREEN_LAYOUT_PARAMS = new FrameLayout.LayoutParams(
-//       LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT, Gravity.CENTER);
-
-//   private WebChromeClient.CustomViewCallback mCustomViewCallback;
-
-//   private Activity mActivity;
-//   private View mWebView;
-//   private View mVideoView;
-
-//   public VideoWebChromeClient(Activity activity, WebView webView) {
-//     mWebView = webView;
-//     mActivity = activity;
-//   }
-
-//   @Override
-//   public void onShowCustomView(View view, CustomViewCallback callback) {
-//     if (mVideoView != null) {
-//       callback.onCustomViewHidden();
-//       return;
-//     }
-
-//     // Store the view and it's callback for later, so we can dispose of them correctly
-//     mVideoView = view;
-//     mCustomViewCallback = callback;
-
-//     view.setBackgroundColor(Color.BLACK);
-
-//     getRootView().addView(view, FULLSCREEN_LAYOUT_PARAMS);
-
-//     mWebView.setVisibility(View.GONE);
-//   }
-
-//   @Override
-//   public void onHideCustomView() {
-//     if (mVideoView == null) {
-//       return;
-//     }
-
-//     mVideoView.setVisibility(View.GONE);
-
-//     // Remove the custom view from its container.
-//     getRootView().removeView(mVideoView);
-//     mVideoView = null;
-//     mCustomViewCallback.onCustomViewHidden();
-
-//     mWebView.setVisibility(View.VISIBLE);
-//   }
-
-//   private ViewGroup getRootView() {
-//     return ((ViewGroup) mActivity.findViewById(android.R.id.content));
-//   }
-// }
